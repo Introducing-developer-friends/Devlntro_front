58399d3b5153f81956c6085d3ed11142b30d673d
oasisgorilla <yongjae0522@gmail.com>
2024-09-17 18:05:00 +0900

    prettier적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3000')
-      .then(response => setMessage(response.data))
-      .catch(error => console.error('Error:', error))
-  }, [])
+    axios
+      .get("http://localhost:3000")
+      .then((response) => setMessage(response.data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
     <div className="App">
       <h1>{message}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
