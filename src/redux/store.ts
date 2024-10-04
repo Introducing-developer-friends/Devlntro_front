c51a17388b58f39daa216b9b4280c6f8ec06c6ec
doyeon kim <kim2004on@gmail.com>
2024-10-04 13:09:51 +0900

    Feat: 리덕스로 알람 상태 관리 구현 - 조회, 읽음, 삭제, 일괄 삭제 구현

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import notificationReducer from './notificationSlice';
 
-// 스토어 설정
 export const store = configureStore({
   reducer: {
-    user: userReducer, // 사용자 상태를 관리하는 리듀서
+    user: userReducer,
+    notifications: notificationReducer,
   },
 });
 
-// 스토어 타입 설정
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
